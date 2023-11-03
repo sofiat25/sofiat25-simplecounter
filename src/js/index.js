0d49fe062e228421f40e41f60191f43530c7d564
Sofia Vanessa <105688383+sofiat25@users.noreply.github.com>
2023-11-03 20:52:44 +0000

    cambios

--- src/js/index.js
+++ src/js/index.js
@@ -9,31 +9,17 @@ import "../styles/index.css";
 
 //import your own components
 import SimpleCounter from "./component/home.jsx";
-let seg, seg2, min, min2, hour, hour2 
-
-seg = seg2 = min = min2 = hour = hour2 = 0;
+let counter = 0;
 setInterval(function() {
-		seg++
-	if (seg == 9){
-		seg = 0
-		seg2++
-	}
-	if (seg2 == 6) {
-		seg = 0
-		seg2 = 0
-		min++
-	}
-	if ( min == 9){
-		seg = 0
-		seg2 = 0
-		min=0
-		min2++
-	}
+	const  four = Math.floor(counter/1000);
+	const  three = Math.floor(counter/100);
+	const  two = Math.floor(counter/10);
+	const  one = Math.floor(counter/1);
+console.log(four,three, two, one); 
 
-
-
+counter++; 
 ReactDOM.render(
-	<SimpleCounter  digitOne={ seg} digitTwo={seg2} digitThree={min} digitFour={min2}/>,
+	<SimpleCounter  digitOne={ one} digitTwo={two} digitThree={three} digitFour={four} />,
 	document.querySelector("#app"));
 	
 }, 1000);
