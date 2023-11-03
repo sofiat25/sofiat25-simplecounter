bcac76acb0e5718beb8034e593458322241fcd22
Sofia Vanessa <105688383+sofiat25@users.noreply.github.com>
2023-11-03 01:41:15 +0000

    cambios

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,10 +9,10 @@ function  SimpleCounter(props)  {
 		<div className="counter">
     <div className="col"><i className="far fa-clock"></i>	
     </div>
-    <div className="col-four">{props.digitFour %10}</div>
-    <div className="col-three">{props.digitThree %10}</div>
-	<div className="col-two">{props.digitTwo % 10}</div>
-	<div className="col-one">{props.digitOne %10}</div>
+    <div className="col-four">{props.digitFour}</div>
+    <div className="col-three">{props.digitThree }</div>
+	<div className="col-two">{props.digitTwo }</div>
+	<div className="col-one">{props.digitOne }</div>
   </div>);
 }
 SimpleCounter.propTypes ={
@@ -21,21 +21,6 @@ digitThree: PropTypes.number,
 digitTwo: PropTypes.number,
 digitOne: PropTypes.number
 };
-
-let counter = 0;
-setInterval(function() {
-	const  four = Math.floor(counter/1000);
-	const  three = Math.floor(counter/100);
-	const  two = Math.floor(counter/10);
-	const  one = Math.floor(counter/1);
-console.log(four,three, two, one); 
-
-counter++; 
-ReactDOM.render(
-	<SimpleCounter  digitOne={ one} digitTwo={two} digitThree={three} digitFour={four} />,
-	document.querySelector("#app"));
-	
-}, 1000);
 
 
 
--- src/js/index.js
+++ src/js/index.js
@@ -9,19 +9,43 @@ import "../styles/index.css";
 
 //import your own components
 import SimpleCounter from "./component/home.jsx";
+let seg, seg2, min, min2, hour, hour2 
 
-let counter = 0;
+seg = seg2 = min = min2 = hour = hour2 = 0;
+setInterval(function() {
+		seg++
+	if (seg == 9){
+		seg = 0
+		seg2++
+	}
+	if (seg2 == 6) {
+		seg = 0
+		seg2 = 0
+		min++
+	}
+	if ( min == 9){
+		seg = 0
+		seg2 = 0
+		min=0
+		min2++
+	}
 
-	const  four = Math.floor(counter/1000);
-	const  three = Math.floor(counter/100);
-	const  two = Math.floor(counter/10);
-	const  one = Math.floor(counter/1);
-console.log(four,three, two, one); 
+
+
+ReactDOM.render(
+	<SimpleCounter  digitOne={ seg} digitTwo={seg2} digitThree={min} digitFour={min2}/>,
+	document.querySelector("#app"));
+	
+}, 1000);
 
 
 
+// const  four = Math.floor(counter/1000);
+// 	const  three = Math.floor(counter/100);
+// 	const  two = Math.floor(counter/10);
+// 	const  one = Math.floor(counter/1);
+// console.log(four,three, two, one); 
 
-//render your react application
-ReactDOM.render(<SimpleCounter digitFour={ four} digitThree={ three} digitTwo={two} digitOne={ one}/>,
-document.querySelector("#app"));
+// counter++; 
 
+// digitThree={three} digitFour={four}
